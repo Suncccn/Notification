1f457c3a85766d32822e3115c38f0897dd09aed5
Suncccn <15720623130@163.com>
2019-12-14 10:32:37 +0800

    Initial commit

---
+++ app/src/main/java/com/scc/notification/MainActivity.java
@@ -0,0 +1,23 @@
+package com.scc.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void del(View v){
+
+    }
+
+    public void send(View v){
+
+    }
+}
